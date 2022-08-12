6cf4b0bcab0bab11cec4e9e71b83e5e425a8e132
djcartwrightweb <dj.cartwright.web@gmail.com>
2022-08-12 13:42:52 -0400

    start game loop

--- scripts.js
+++ scripts.js
@@ -44,13 +44,43 @@ function playGame(playerSelection, computerSelection) {
     }
 }
 
-let computerSelection = getComputerChoice();
-
-function game() {
+let playerScore = 0;
+let computerScore = 0;
+/*function game() {
     for (let i = 0; i < 5; i ++) {
     console.log(playGame(prompt('Rock, paper, or scissors? ' + i + 1), getComputerChoice()));
     }
-}
+}*/
 
-game();
+//create and add buttons with event-listeners to DOM
+const rockButton = document.createElement('button');
+rockButton.textContent = 'rock';
+rockButton.addEventListener('click', (e) => {
+    results.innerHTML = playGame('rock', getComputerChoice());
+});
+
+const paperButton = document.createElement('button');
+paperButton.textContent = 'paper';
+paperButton.addEventListener('click', (e) => {
+    results.innerHTML = playGame('paper', getComputerChoice());
+});
+
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'scissors';
+scissorsButton.addEventListener('click', (e) => {
+    results.innerHTML = playGame('scissors', getComputerChoice());
+});
+
+document.body.appendChild(rockButton);
+document.body.appendChild(paperButton);
+document.body.appendChild(scissorsButton);
+
+
+//creating and appending elements to show results of games
+
+const results = document.createElement('div');
+document.body.appendChild(results);
+results.innerHTML = 'results...';
+
+//game();
 
